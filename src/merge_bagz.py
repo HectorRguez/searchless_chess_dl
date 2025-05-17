6903abd6bba79c133a4433e7c8db9f94ec50e552
HectorRguez <hectorrodriguezrodriguez52@gmail.com>
2025-05-17 22:03:33 +0800

    First working version for state-value

---
+++ src/merge_bagz.py
@@ -0,0 +1,8 @@
+from searchless_chess.src import BagFileReader, BagFileWriter
+
+def merge_bagz_files(input_files, output_file):
+    with BagWriter(output_file) as writer:
+        for input_file in input_files:
+            reader = BagReader(input_file)
+            for i in range(len(reader)):
+                writer.write(reader[i])
